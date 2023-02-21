23ead2134108c696bc725f83548e6ba71ad59a07
Xian L <xianl@Xians-MacBook-Air.local>
2023-02-21 11:03:52 -0600

    edge function vercel

---
+++ edge-entry.js
@@ -0,0 +1,19 @@
+import handleRequest from './src/App.server';
+import indexTemplate from './dist/client/index.html?raw';
+
+// ReadableStream is bugged in Vercel Edge, overwrite with polyfill
+import {ReadableStream} from 'web-streams-polyfill/ponyfill';
+Object.assign(globalThis, {ReadableStream});
+
+export default async (request, event) => {
+  try {
+    return await handleRequest(request, {
+      indexTemplate,
+      context: event,
+    });
+  } catch (error) {
+    return new Response(error.message || error.toString(), {
+      status: 500,
+    });
+  }
+};
